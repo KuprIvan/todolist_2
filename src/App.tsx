b854afd40101b46651b6b0f5c61270527e3a0dbe
ivankupryianau <ivan.kupryianau@capital.com>
2023-03-06 17:41:44 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Todolist from "./Components/Todolist";
+
+function App() {
+    const task1 = [
+        { id: 1, title: 'HTML&CSS', isDone: true },
+        { id: 2, title: 'JS', isDone: true },
+        { id: 3, title: 'ReactJS', isDone: false },
+    ]
+    const task2 = [
+        { id: 1, title: 'Hello World', isDone: true },
+        { id: 2, title: 'I am happy', isDone: true },
+        { id: 3, title: 'Yo', isDone: false },
+    ]
+
+
+
+    return (
+        <div className="App">
+            <Todolist title="What to learn" tasks={task1} />
+            <Todolist title="Songs" tasks={task2} />
+        </div>
+    );
+}
+
+export default App;
